c68fcbf3f9b8feb51e5380e48627244364676b69
Dheeraj <134566303+upraity@users.noreply.github.com>
2023-09-27 19:24:37 +0530

    Create Q367_Valid_Perfect_Square.c

---
+++ problems/leetcode/Q367_Valid_Perfect_Square.c
@@ -0,0 +1,12 @@
+bool isPerfectSquare(int num){
+    int r;
+    #include<math.h>
+    r = pow(num,0.5);
+    if(r*r==num)
+    {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
