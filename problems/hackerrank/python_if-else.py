87c0f50e9eb2eb55ced60d7eb9fae20d7288eb3b
Dheeraj <134566303+upraity@users.noreply.github.com>
2023-09-27 19:52:36 +0530

    Create python_if-else.py

---
+++ problems/hackerrank/python_if-else.py
@@ -0,0 +1,9 @@
+if n % 2 != 0: # odd
+    print("Weird")
+if n % 2 == 0: # even 
+    if 2 < n < 5:
+        print("Not Weird")
+    if 6 < n <= 20:
+        print("Weird")
+    if n > 20:
+        print("Not Weird")
