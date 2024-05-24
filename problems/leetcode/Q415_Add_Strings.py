f6bfa5600a59e0520525f1a4a697b7270c842746
Dj <134566303+upraity@users.noreply.github.com>
2024-05-24 20:55:17 +0530

    Create Q415_Add_Strings.py

---
+++ problems/leetcode/Q415_Add_Strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def addStrings(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        num1 = int(num1)
+        num2 = int(num2)
+        ans = str(num1 + num2)
+        return ans
