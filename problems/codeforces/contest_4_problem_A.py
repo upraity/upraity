9b411e5cacb3ab323c1d0c19d0f3e9c8d2a91e2e
Dj <134566303+upraity@users.noreply.github.com>
2024-05-31 20:15:07 +0530

    Create contest_4_problem_A.py

---
+++ problems/codeforces/contest_4_problem_A.py
@@ -0,0 +1,9 @@
+def watermelon(n):
+    if(n != 2):
+        if(n%2 == 0):
+            print('yes')
+        else:
+            print('no')
+    else:
+        print('no')
+watermelon(int(input()))
