3017707340d78e05c5d62a34f68095b93b9c7fed
Dj <134566303+upraity@users.noreply.github.com>
2025-03-26 20:59:47 +0530

    Create Q.405_Convert_a_Number_to_Hexadecimal.php

---
+++ problems/leetcode/Q.405_Convert_a_Number_to_Hexadecimal.php
@@ -0,0 +1,12 @@
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @return String
+     */
+    function toHex($num) {
+        // Handle two's complement for negative numbers
+        $num = $num & 0xFFFFFFFF;
+        return dechex($num);
+    }
+}
