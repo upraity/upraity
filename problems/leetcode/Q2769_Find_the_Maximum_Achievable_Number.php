d8f8934b7f988a5541cc8a77479a469d5fde2769
Dj <134566303+upraity@users.noreply.github.com>
2025-04-01 22:04:38 +0530

    Create Q2769_Find_the_Maximum_Achievable_Number.php

---
+++ problems/leetcode/Q2769_Find_the_Maximum_Achievable_Number.php
@@ -0,0 +1,11 @@
+class Solution {
+
+    /**
+     * @param Integer $num
+     * @param Integer $t
+     * @return Integer
+     */
+    function theMaximumAchievableX($num, $t) {
+        return $num + (2*$t);
+    }
+}
